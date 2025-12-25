a1fc0e2e3013995fb2ef2978a05a9251cdd15a4f
laxmi-22 <77582871+laxmi-22@users.noreply.github.com>
2025-12-26 00:52:56 +0530

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -2,12 +2,13 @@
 from dotenv import load_dotenv
 import os
 from langchain_groq import ChatGroq
-
+from dotenv import load_dotenv
 
 #loading api key (environment variable)
 load_dotenv()
 
 # create instance of llm by providing API key
 llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.3-70b-versatile")
+
 
 
